822d5b81f4929273e7804f3c8d98aa1d84f2339b
CrackedOnTiti <thierry.bungaroo@epitech.eu>
2025-12-23 18:27:06 +0100

    [ADD] initialising the Oauth for github in app.py #67

--- server/app.py
+++ server/app.py
@@ -49,6 +49,18 @@ oauth.register(
     }
 )
 
+oauth.register(
+    name='github',
+    client_id=Config.GITHUB_CLIENT_ID,
+    client_secret=Config.GITHUB_CLIENT_SECRET,
+    authorize_url='https://github.com/login/oauth/authorize',
+    access_token_url='https://github.com/login/oauth/access_token',
+    api_base_url='https://api.github.com/',
+    client_kwargs={
+        'scope': 'repo read:user user:email'
+    }
+)
+
 # create tables (basically mkdir -p)
 with app.app_context():
     db.create_all()

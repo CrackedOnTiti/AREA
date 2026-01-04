3dd2e3735b107787a3f961bf5c38309bebd773dd
CrackedOnTiti <thierry.bungaroo@epitech.eu>
2026-01-04 03:17:26 +0100

    [ADD] barebones testing for flutter apk temporary

---
+++ client/apk/lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/widgets.dart';
+
+void main() {
+  runApp(
+    Container(
+      color: Color(0xFFFFFFFF),
+      child: Text('Hello World', textDirection: TextDirection.ltr),
+    ),
+  );
+}

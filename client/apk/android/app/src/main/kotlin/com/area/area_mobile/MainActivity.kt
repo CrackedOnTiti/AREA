7299047445f98394390b65f9e37b61ae7f5ae034
CrackedOnTiti <thierry.bungaroo@epitech.eu>
2026-01-09 19:04:42 +0100

    [ADD] returned apk from debug to release and removed AREA title bar UI #84

--- client/apk/android/app/src/main/kotlin/com/area/area_mobile/MainActivity.kt
+++ client/apk/android/app/src/main/kotlin/com/area/area_mobile/MainActivity.kt
@@ -1,5 +1,12 @@
 package com.area.area_mobile
 
 import io.flutter.embedding.android.FlutterActivity
+import android.os.Bundle
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // Hide the action bar
+        actionBar?.hide()
+    }
+}

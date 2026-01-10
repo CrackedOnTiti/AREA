f8cfdf99bde93c4be608e60a707d6cdca666b0da
CrackedOnTiti <thierry.bungaroo@epitech.eu>
2026-01-10 16:36:51 +0100

    [UPD] home_screen now has a titlebar that displays the logo and a menu icon on the right #86

--- client/apk/lib/screens/home_screen.dart
+++ client/apk/lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import '../services/storage_service.dart';
 import 'login_screen.dart';
 
@@ -21,25 +22,94 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-      body: Center(
+      body: SafeArea(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(
-              'Home Screen',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 24,
+            // Top header container
+            Container(
+              width: double.infinity,
+              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+              decoration: BoxDecoration(
+                color: Colors.black,
+                border: Border.all(color: Colors.white, width: 1),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  // AREA logo on the left
+                  Container(
+                    padding: EdgeInsets.symmetric(horizontal: 3, vertical: 3),
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.white, width: 2),
+                    ),
+                    child: Text(
+                      'AR\nEA',
+                      style: GoogleFonts.russoOne(
+                        fontSize: 20,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        height: 0.8,
+                        letterSpacing: 2,
+                      ),
+                    ),
+                  ),
+                  // Hamburger menu icon on the right
+                  GestureDetector(
+                    onTap: () {
+                      print('Menu pressed');
+                      // TODO: Open menu
+                    },
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Container(
+                          width: 25,
+                          height: 3,
+                          color: Colors.white,
+                        ),
+                        SizedBox(height: 4),
+                        Container(
+                          width: 25,
+                          height: 3,
+                          color: Colors.white,
+                        ),
+                        SizedBox(height: 4),
+                        Container(
+                          width: 25,
+                          height: 3,
+                          color: Colors.white,
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
               ),
             ),
-            SizedBox(height: 40),
-            ElevatedButton(
-              onPressed: () => _handleLogout(context),
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.white,
-                foregroundColor: Colors.black,
+            // Rest of the screen
+            Expanded(
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Home',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 24,
+                      ),
+                    ),
+                    SizedBox(height: 40),
+                    ElevatedButton(
+                      onPressed: () => _handleLogout(context),
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.white,
+                        foregroundColor: Colors.black,
+                      ),
+                      child: Text('Logout'),
+                    ),
+                  ],
+                ),
               ),
-              child: Text('Logout'),
             ),
           ],
         ),
